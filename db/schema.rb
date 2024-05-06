380fcb34f91ca6ab5917542bebfdb9d1eb85250f
touchtak <159424108+touchtak@users.noreply.github.com>
2024-05-06 12:43:08 +0000

    created top/registration views

--- db/schema.rb
+++ db/schema.rb
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2024_05_01_082952) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
