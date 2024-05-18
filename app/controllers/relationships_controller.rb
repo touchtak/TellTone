5b87c46365b07bc6a3d98aef92c632c3eefc53d0
touchtak <159424108+touchtak@users.noreply.github.com>
2024-05-18 08:12:17 +0000

    adopted Ajax with follow system

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -10,11 +10,14 @@ class RelationshipsController < ApplicationController
   end
 
   def viewer_relation_create
+    @viewer = Viewer.find(params[:id])
     viewer_relationship = ViewerRelationship.new
     viewer_relationship.follower_id = current_user.viewer.id
-    viewer_relationship.followed_id = Viewer.find(params[:id]).id
+    viewer_relationship.followed_id = @viewer.id
     viewer_relationship.save
-    redirect_to request.referer
+
+    # 非同期通信用
+    @followings = (@viewer.viewer_followings + @viewer.creator_followings)
   end
 
   # フォロー解除時の処理
@@ -27,7 +30,10 @@ class RelationshipsController < ApplicationController
   def viewer_relation_destroy
     viewer_relationship = ViewerRelationship.find_by(follower_id: current_user.viewer.id, followed_id: params[:id])
     viewer_relationship.destroy
-    redirect_to request.referer
+
+    # 非同期通信用
+    @viewer = Viewer.find(params[:id])
+    @followings = (@viewer.viewer_followings + @viewer.creator_followings)
   end
 
   # 一覧表示
